b95bd3547d60baed329a1b0283070616a5dc04b8
Daniel Anjos <dmartins.anj@gmail.com>
2020-05-25 18:18:31 -0300

    Criado o visual do dashboard

---
+++ public/js/charts.js
@@ -0,0 +1,184 @@
+const primeiroChart = document.getElementById('primeiroChart');
+customPrimeiroChart = new Chart(primeiroChart, {
+  type: 'bar',
+  data: {
+    labels: ['Janeiro', 'Fecereiro', 'Março', 'Abril', 'Maio'],
+    datasets: [
+      {
+        label: '# de tarefas',
+        data: [30, 55, 87, 60, 27],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 99, 132, 1)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    legend: {
+        display: false,
+        labels: {
+            boxWidth: 15
+        }
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    }
+  }
+});
+
+const segundoChart = document.getElementById('segundoChart');
+customSegundoChart = new Chart(segundoChart, {
+  type: 'line',
+  data: {
+    labels: ['Janeiro', 'Fecereiro', 'Março', 'Abril', 'Maio'],
+    datasets: [
+      {
+        label: '# de tarefas',
+        data: [30, 55, 87, 60, 27],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.0)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# de tarefas',
+        data: [80, 70, 12, 60, 27],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.0)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    legend: {
+        display: false,
+        labels: {
+            boxWidth: 15
+        }
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    }
+  }
+});
+
+const terceiroChart = document.getElementById('terceiroChart');
+customTerceiroChart = new Chart(terceiroChart, {
+  type: 'bar',
+  data: {
+    labels: ['Janeiro', 'Fecereiro', 'Março', 'Abril', 'Maio'],
+    datasets: [
+      {
+        label: '# de tarefas',
+        data: [30, 55, 87, 60, 27],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 99, 132, 1)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+      }
+    ],
+    
+  },
+  options: {
+    responsive: true,
+    legend: {
+        display: false,
+        labels: {
+            boxWidth: 15
+        }
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    }
+  }
+});
+
+const quartoChart = document.getElementById('quartoChart');
+customQuartoChart = new Chart(quartoChart, {
+  type: 'line',
+  data: {
+    labels: ['Janeiro', 'Fecereiro', 'Março', 'Abril', 'Maio'],
+    datasets: [
+      {
+        label: '# de tarefas',
+        data: [30, 55, 87, 60, 27],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.0)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# de tarefas',
+        data: [80, 70, 12, 60, 27],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.0)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    legend: {
+        display: false,
+        labels: {
+            boxWidth: 15
+        }
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    }
+  }
+});
--- view/dashboard/dashboard.php
+++ view/dashboard/dashboard.php
@@ -1,5 +1,68 @@
 <?php include __DIR__ . '/../cabecalho.php'; ?>
 
-  <h1>Bem-Vindo</h1>
+<style>
+  .content-dashboard ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 24px;
+    list-style: none;
+  }
+</style>
+
+<section class="content-dashboard mt-5">
+  <ul>
+    <li>
+      <section class="painel">
+        <div class="content-painel">
+          <header class="content-painel-header">
+            <h4>Titulos Cadastrados nos ultimos 6 meses</h4>
+          </header>
+          <canvas id="primeiroChart"></canvas>
+        </div>
+      </section>
+    </li>
+    <li>
+      <section class="painel">
+        <div class="content-painel">
+          <header class="content-painel-header">
+            <h4>Vigentes x Quitados (6 meses)</h4>
+          </header>
+          <canvas id="segundoChart"></canvas>
+        </div>
+      </section>
+    </li>
+    <li>
+      <section class="painel">
+        <div class="content-painel">
+          <header class="content-painel-header">
+            <h4>Valores dos titulos (6 meses)</h4>
+          </header>
+          <canvas id="terceiroChart"></canvas>
+        </div>
+      </section>
+    </li>
+    <li>
+      <section class="painel">
+        <div class="content-painel">
+          <header class="content-painel-header">
+            <h4>Valores Vigentes x Valores Quitados</h4>
+          </header>
+          <canvas id="quartoChart"></canvas>
+        </div>
+      </section>
+    </li>
+  </ul>
+
+</section>
+
+
+<!--  -->
+
+
+
+<script type="text/jscript" src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script type="text/jscript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+<script type="text/jscript" src="/js/charts.js"></script>
+
 
 <?php include __DIR__ . '/../rodape.php'; ?>
